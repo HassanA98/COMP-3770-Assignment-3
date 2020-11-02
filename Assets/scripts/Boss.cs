557c99248261a86084a6d7da6a887bf01015e9ac
mattpotter2442 <73261257+mattpotter2442@users.noreply.github.com>
2020-11-02 12:29:19 -0500

    Update Boss.cs

--- Assets/scripts/Boss.cs
+++ Assets/scripts/Boss.cs
@@ -16,7 +16,7 @@ public class Boss : MonoBehaviour
     public MoonkinDruid dmg3;
 
     private int random;
-    private int totalDmg;
+    public int totalDmg;
 
     // Start is called before the first frame update
     void Start()
