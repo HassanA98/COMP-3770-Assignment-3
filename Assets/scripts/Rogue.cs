f94fdbe4f7453158d477aa0e8cfedf8f521bf394
mattpotter2442 <73261257+mattpotter2442@users.noreply.github.com>
2020-11-02 12:31:09 -0500

    Update Rogue.cs

--- Assets/scripts/Rogue.cs
+++ Assets/scripts/Rogue.cs
@@ -29,11 +29,9 @@ public class Rogue : MonoBehaviour
         if (isDead == false)
             dealDamage();
 
-         if (health <= 0) {
+         if (health <= 0) 
             isDead = true;
-            PlayerPrefs.SetInt("RogueDmg", totalDmg);
-            PlayerPrefs.Save();
-        }
+          
     }
 
     public void dealDamage()
