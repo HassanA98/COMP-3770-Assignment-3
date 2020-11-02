0db8a785069dfae4ce8f54b318447ca3610ccabd
mattpotter2442 <73261257+mattpotter2442@users.noreply.github.com>
2020-11-02 12:31:26 -0500

    Update Warrior.cs

--- Assets/scripts/Warrior.cs
+++ Assets/scripts/Warrior.cs
@@ -29,11 +29,9 @@ public class Warrior : MonoBehaviour
         if (isDead == false)
             dealDamage();
 
-        if (health <= 0) {
+        if (health <= 0) 
             isDead = true;
-            PlayerPrefs.SetInt("WarriorDmg", totalDmg);
-            PlayerPrefs.Save();
-        }
+         
     }
 
     public void dealDamage()
